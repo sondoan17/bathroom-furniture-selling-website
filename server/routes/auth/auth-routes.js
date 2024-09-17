1fb6a4f15484ebf9c8ac4fd1ebef389357587a62
sondoan17 <sondoan17@gmail.com>
2024-09-17 20:25:57 +0700

    update

--- server/routes/auth/auth-routes.js
+++ server/routes/auth/auth-routes.js
@@ -8,18 +8,11 @@ const {
 } = require("../../controllers/auth/auth-controller");
 
 const router = express.Router();
-const allowedOrigins = [process.env.FRONTEND_URL];
 
-const corsOptions = {
-  origin: allowedOrigins,
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register",  registerUser);
+router.post("/login",  loginUser);
 router.post("/logout", logoutUser);
-router.get("/check-auth", cors(corsOptions), authMiddleware, (req, res) => {
+router.get("/check-auth", authMiddleware, (req, res) => {
   const user = req.user;
   res.status(200).json({
     success: true,
