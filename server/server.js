0ab7b93d4b47ad5755782fdcef97c6c6c62fd5e7
sondoan17 <sondoan17@gmail.com>
2024-09-17 14:45:41 +0700

    update

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: '*',
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
