6bc06fef6a9c772837ae485e4fe7715c0c090d9d
sondoan17 <sondoan17@gmail.com>
2024-09-17 15:54:39 +0700

    update

--- server/server.js
+++ server/server.js
@@ -23,29 +23,20 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigin = process.env.FRONTEND_URL;
-
-app.use(cors({
-  origin: function (origin, callback) {
-   
-    const cleanOrigin = origin ? origin.replace(/\/$/, '') : '';
-    if (!origin || cleanOrigin === allowedOrigin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200
-}));
-
-// Add a middleware to ensure correct CORS headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', allowedOrigin);
-  next();
-});
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
