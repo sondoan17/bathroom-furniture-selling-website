9c141bd6090b4c8e136b3e57af9f8ff2d3752129
sondoan17 <sondoan17@gmail.com>
2024-09-24 15:22:59 +0700

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { Toaster } from "./components/ui/toaster.jsx";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
+      <Analytics />
       <App />
       <Toaster />
     </Provider>
